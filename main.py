be3ea9c5b4a4e9baaaca59bd1bd8cdc872673d72
Alpay Alın <alpay.aln@insider.local>
2021-10-14 15:15:39 +0300

    Freshly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class WebPush:
 
-    def __init__(self, Platform, optin, global_frequency_capping, start_date, end_date, language, push_type):
+    def __init__(self, Platform = "Chrome", optin = "True", global_frequency_capping = "Three_views_in_24hours", start_date ="09.19.2021", end_date = "10.19.2021", language = "ENG", push_type = "Web Push"):
 
         self.Platform = Platform
         self.optin = optin
@@ -15,76 +15,76 @@ class WebPush:
 
 class TriggerPush(WebPush):
 
-    def __init__(self, Platform, optin, global_frequency_capping, start_date, end_date, language, push_type):
+    def __init__(self, push_type):
 
-        super().__init__(Platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
+        self.push_type = push_type
         Trigger_page = "Homepage"
         WebPush.send_push(self)
 
 class BulkPush(WebPush):
 
-    def __init__(self, Platform, optin, global_frequency_capping, start_date, end_date, language, push_type):
+    def __init__(self, push_type):
 
-        super().__init__(Platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
+        self.push_type = push_type
         send_date = int(51)
         WebPush.send_push(self)
 
 class SegmentPush(WebPush):
 
-    def __init__(self, Platform, optin, global_frequency_capping, start_date, end_date, language, push_type):
+    def __init__(self, push_type):
 
-        super().__init__(Platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
+        self.push_type = push_type
+
         segment_name = str("Book Searchers")
         WebPush.send_push(self)
 
 class PriceAlertPush(WebPush):
 
-    @staticmethod
     def discountPrice(price_info, discount_rate):
 
-
+        price_info = price_info
         discount_rate = price_info * discount_rate
 
         return discount_rate
 
 class InstockPush(WebPush):
 
-    def __init__(self, Platform, optin, global_frequency_capping, start_date, end_date, language, push_type):
+    def __init__(self, push_type):
 
-        super().__init__(Platform, optin, global_frequency_capping, start_date, end_date, language, push_type)
-        WebPush.send_push(self)
+        self.push_type = push_type
 
     @staticmethod
     def stockUpdate(stock_info):
+        print(type(stock_info))
+        print(stock_info)
+        stock_info = stock_info.lower()
 
+        if (stock_info == "true"):
+            stock_info = "false"
 
-        if (stock_info == True):
-
-            stock_info = False
-            print(stock_info)
-
-        elif(stock_info == False)
-
-            stock_info = True
-            print(stock_info)
+        elif (stock_info == "false"):
+            stock_info = "true"
 
         return stock_info
 
-Web_Push_Nesne = WebPush("Chrome", True, "Three_views_in_24hours", "09.19.2021", "10.19.2021", "ENG", "Trigger Push")
-Trigger_Push_Nesne = TriggerPush("Chrome", True, "Three_views_in_24hours", "09.19.2021", "10.19.2021", "ENG", "Trigger Push")
-Bulk_Push_Nesne = BulkPush("Chrome", True, "Three_views_in_24hours", "09.19.2021", "10.19.2021", "ENG", "Bulk Push")
-Segment_Push_Nesne = SegmentPush("Chrome", True, "Three_views_in_24hours", "09.19.2021", "10.19.2021", "ENG", "Segment Push")
+Web_Push_Nesne = WebPush("Web Push")
+Web_Push_Nesne.send_push()
+Trigger_Push_Nesne = TriggerPush("Trigger Push")
+Bulk_Push_Nesne = BulkPush("Bulk Push")
+Segment_Push_Nesne = SegmentPush("Segment Push")
 
 PriceInfo_ = int(input("Please enter Price Info:"))
 DiscountRate_ = float(input("Please enter Discount Rate:"))
-stock_info = bool(input("Please enter Stock Info Update:"))
+stock_info = input("Please enter Stock Info Update:")
 
 
-Instock_Push_Nesne = InstockPush("Chrome", True, "Three_views_in_24hours", "09.19.2021", "10.19.2021", "ENG", "Instock Push")
-Instock_Push_Nesne.stockUpdate(stock_info)
+Instock_Push_Nesne = InstockPush("Instock Push")
+updated = Instock_Push_Nesne.stockUpdate(stock_info)
 
+print(updated)
 
 AbouttheDiscount = PriceAlertPush.discountPrice(PriceInfo_, DiscountRate_)
+
 print(AbouttheDiscount)
 
 
